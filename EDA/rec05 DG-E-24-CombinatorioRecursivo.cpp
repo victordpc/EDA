ab06b8ea2ae43f9bc1db19238e6bef0ad320a978
victordpc <victor@eduroam098195.eduroam.ucm.es>
2016-12-13 13:12:28 +0100

    Recursivos

---
+++ EDA/rec05 DG-E-24-CombinatorioRecursivo.cpp
@@ -0,0 +1,54 @@
+////
+////  rec05.cpp
+////  EDA
+////
+////  Created by Victor del Pino Castilla on 11/12/16.
+////  Copyright © 2016 Victor del Pino Castilla. All rights reserved.
+////
+//
+//#include <stdio.h>
+//#include <iostream>
+//
+//using namespace std;
+//
+//int recursivo(int arriba, int abajo, int** m);
+//
+//int main(){
+//    int arriba=0, abajo=0;
+//    int** m;
+//    
+//    cin >> arriba;
+//    while (0 < arriba){
+//        cin >> abajo;
+//        
+//        m=new int*[arriba+1];
+//
+//        for (int i=0; i<=arriba; i++) {
+//            m[i]=new int[abajo+1];
+//
+//            for (int j=0; j<=abajo; j++) {
+//                m[i][j]=-1;
+//            }
+//        }
+//
+//        cout << recursivo(arriba,abajo,m) << endl;
+//
+//        for (int i=0; i<arriba; i++) {
+//            delete m[i];
+//        }
+//        delete m;
+//
+//        cin >> arriba;
+//    }
+//}
+//
+//
+//int recursivo(int arriba, int abajo, int** m){
+//    if(m[arriba][abajo] != -1)return m[arriba][abajo];
+//    else if (abajo == 0) return 1;
+//    else if (arriba == 0) return 0;
+//    else{
+//        m[arriba][abajo] = recursivo(arriba - 1, abajo - 1, m) + recursivo(arriba - 1, abajo, m);
+//        return m[arriba][abajo];
+//    }
+//}
---
+++ EDA/rec06.cpp
@@ -0,0 +1,23 @@
+//
+//  rec06.cpp
+//  EDA
+//
+//  Created by Victor del Pino Castilla on 13/12/16.
+//  Copyright © 2016 Victor del Pino Castilla. All rights reserved.
+//
+
+#include <stdio.h>
+#include "iostream"
+
+using namespace std;
+
+int main(){
+    int n = 0, d = 0;
+    cin >> n;
+    cin >> d;
+    
+    while (0 < n) {
+        cin >> n;
+        cin >> d;
+    }
+}
