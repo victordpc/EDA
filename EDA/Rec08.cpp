34224d8833602501d06c5752eadba371bced9f56
victordpc <victor@MacBook-Pro-de-Victor.local>
2016-12-15 00:55:29 +0100

    Changes

--- EDA/Rec08.cpp
+++ EDA/Rec08.cpp
@@ -7,3 +7,75 @@
 //
 
 #include <stdio.h>
+#include "iostream"
+
+using namespace std;
+
+long long int fob(int n, long long int x, long long int y);
+long long int fob(int n, int i, long long int x, long long int y);
+int fob2(int n, int* v);
+int fob2(int n, int x, int y);
+
+int main(){
+    int n = 0, x = 0, y = 0;
+    
+    cin >> n;
+    
+    while (n != -1){
+        cin >> x >> y;
+        
+        if(n == 0){
+            cout << x;
+        } else if (n == 1) {
+            cout << y;
+        } else {
+//            cout << fob(n,x,y);
+            cout << fob2(n,x,y);
+        }
+        cout << endl;
+        
+        n = 0; x = 0; y = 0;
+        cin >> n;
+    }
+}
+
+//recursion no final, lineal
+int fob2(int n, int x, int y){
+    if (n==0) return x;
+    else if(n==1) return y;
+    else
+    {
+        int* v;
+        v = new int[n];
+
+        v[0]=x;
+        v[1]=y;
+        for (int i = 2; i<=n; i++) {
+            v[i]=-1;
+        }
+
+        return fob2(n, v);
+    }
+}
+
+int fob2(int n, int* v){
+    if(v[n] != -1) return v[n];
+    else{
+        v[n] = fob2(n-1,v)+fob2(n-2,v);
+        return v[n];
+    }
+}
+
+//recursion final, lineal en tiempo
+long long int fob(int n, long long int x, long long int y){
+    if (n==0) return x;
+    else if(n==1) return y;
+    else
+        return fob(n,1,y,x);
+}
+
+long long int fob(int n, int i, long long int x,long long int y){
+    if (n==i) return x;
+    else
+        return fob(n,i+1,x+y,x);
+}
--- EDA/iter09 particion.cpp
+++ EDA/iter09 particion.cpp
@@ -1,72 +1,72 @@
-#include<iostream>
-#include<fstream>
-using namespace std;
-
-//const int N = 1000000;
-int nNinos(int n);
-
-int main()
-{
-    int n = 0;
-    
-   /* cin >> n;
-    while(n != 0){
-        cout << nNinos(n) << "\n";
-        
-        cin >> n;
-    }
-    return 0;
-    */
-    
-    
-    int nCasos = 0;
-    cin >> nCasos;
-    for(int i = 0; i < nCasos; i++){
-        cin >> n;
-        cout << nNinos(n) - 1 << "\n";
-
-    }
-}
-
-/*
- 2
- 6
- 324176
- 15
- 2 1 3 1 2 6 5 6 9 10 10 9 13 15 13
- */
-
-
-int nNinos (int n){
-    int i = 0; // 1?
-    int ninos = 0;
-    int acum = 0;
-    int dato;
-    cin >> dato;
-    int maxMin = dato;
-    int max= maxMin;
-    
-    // cout << "maxMin | max | acum | ninos | dato " << endl;
-    while( i < n){
-        
-        if( dato <= maxMin){
-            ninos = i + 1;
-            maxMin = max;
-        }
-        else if(dato > max){//maxMin
-            max = dato;
-        }
-        //  cout << "  " << maxMin << "       " << max << "      " << acum << "      " << ninos << "      " << dato << endl;
-        i++;
-        if(i < n)
-            cin >> dato;
-    }
-    
-    
-    return ninos;
-}
-
-
-
-
-
+//#include<iostream>
+//#include<fstream>
+//using namespace std;
+//
+////const int N = 1000000;
+//int nNinos(int n);
+//
+//int main()
+//{
+//    int n = 0;
+//    
+//   /* cin >> n;
+//    while(n != 0){
+//        cout << nNinos(n) << "\n";
+//        
+//        cin >> n;
+//    }
+//    return 0;
+//    */
+//    
+//    
+//    int nCasos = 0;
+//    cin >> nCasos;
+//    for(int i = 0; i < nCasos; i++){
+//        cin >> n;
+//        cout << nNinos(n) - 1 << "\n";
+//
+//    }
+//}
+//
+///*
+// 2
+// 6
+// 324176
+// 15
+// 2 1 3 1 2 6 5 6 9 10 10 9 13 15 13
+// */
+//
+//
+//int nNinos (int n){
+//    int i = 0; // 1?
+//    int ninos = 0;
+//    int acum = 0;
+//    int dato;
+//    cin >> dato;
+//    int maxMin = dato;
+//    int max= maxMin;
+//    
+//    // cout << "maxMin | max | acum | ninos | dato " << endl;
+//    while( i < n){
+//        
+//        if( dato <= maxMin){
+//            ninos = i + 1;
+//            maxMin = max;
+//        }
+//        else if(dato > max){//maxMin
+//            max = dato;
+//        }
+//        //  cout << "  " << maxMin << "       " << max << "      " << acum << "      " << ninos << "      " << dato << endl;
+//        i++;
+//        if(i < n)
+//            cin >> dato;
+//    }
+//    
+//    
+//    return ninos;
+//}
+//
+//
+//
+//
+//
