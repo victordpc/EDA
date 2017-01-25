7db7b16ec878f32423b214ff4ba5b9ee7beef21d
victordpc <victor@eduroam106011.eduroam.ucm.es>
2017-01-26 00:32:54 +0100

    DyV

---
+++ EDA/DyV04.cpp
@@ -0,0 +1,26 @@
+//
+//  DyV04.cpp
+//  EDA
+//
+//  Created by Victor del Pino Castilla on 26/1/17.
+//  Copyright © 2017 Victor del Pino Castilla. All rights reserved.
+//
+
+#include <stdio.h>
+#include "iostream"
+
+using namespace std;
+
+
+//En este problema diremos que un vector est´a parcialmente ordenado, si el valor
+//m´aximo de su mitad derecha es mayor o igual que todos los valores de la mitad izquierda y el valor
+//m´ınimo de su mitad izquierda es menor o igual que todos los valores de su mitad derecha. Adem´as tanto
+//la mitad izquierda como la derecha cumplen que est´an parcialmente ordenados.
+int main(){
+    bool salir=true;
+    int n=0;
+    int* v;
+    while (salir){
+        
+    }
+}
