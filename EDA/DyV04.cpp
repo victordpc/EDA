1d0f6e8dec1e909359bba618d9e50bf6a33321ce
victordpc <victor@eduroam097176.eduroam.ucm.es>
2017-01-31 11:50:11 +0100

    va05

--- EDA/DyV04.cpp
+++ EDA/DyV04.cpp
@@ -1,26 +1,74 @@
+////
+////  DyV04.cpp
+////  EDA
+////
+////  Created by Victor del Pino Castilla on 26/1/17.
+////  Copyright © 2017 Victor del Pino Castilla. All rights reserved.
+////
 //
-//  DyV04.cpp
-//  EDA
+//#include <stdio.h>
+//#include "iostream"
 //
-//  Created by Victor del Pino Castilla on 26/1/17.
-//  Copyright © 2017 Victor del Pino Castilla. All rights reserved.
+//using namespace std;
 //
-
-#include <stdio.h>
-#include "iostream"
-
-using namespace std;
-
-
-//En este problema diremos que un vector est´a parcialmente ordenado, si el valor
-//m´aximo de su mitad derecha es mayor o igual que todos los valores de la mitad izquierda y el valor
-//m´ınimo de su mitad izquierda es menor o igual que todos los valores de su mitad derecha. Adem´as tanto
-//la mitad izquierda como la derecha cumplen que est´an parcialmente ordenados.
-int main(){
-    bool salir=true;
-    int n=0;
-    int* v;
-    while (salir){
-        
-    }
-}
+//string DyV(int* v, int n);
+//bool DyV(int* v, int i, int j, int& max, int& min);
+//
+//bool DyV(int* v, int i, int j, int& max, int& min){
+//    int medio=(i+j)/2;
+//    
+//    int maximoI=0;
+//    int minimoI=0;
+//    int maximoD=0;
+//    int minimoD=0;
+//    
+//    if(j==i){
+//        max = v[i];
+//        min = v[i];
+//        return true;
+//    }else if(DyV(v, i, medio, maximoI, minimoI) && DyV(v, medio+1, j, maximoD, minimoD)){
+//        
+//        if(maximoI > maximoD) max = maximoI;
+//        else max = maximoD;
+//        
+//        if(minimoI < minimoD) min = minimoI;
+//        else min = minimoD;
+//        
+//        return (maximoD >= maximoI && minimoI <= minimoD);
+//    }else{
+//        return false;
+//    }
+//}
+//
+//string DyV(int* v, int n){
+//    int maximo=0;
+//    int minimo=0;
+//    
+//    if(DyV(v, 0, n-1, maximo, minimo))
+//        return "SI";
+//    else
+//        return "NO";
+//}
+//
+//int main(){
+//    bool segir=true;
+//    int n=0;
+//    int* v= new int[100000];
+//    int numero;
+//    
+//    while (segir){
+//        cin >> numero;
+//        while (numero!=0) {
+//            v[n++]=numero;
+//            cin >> numero;
+//        }
+//        
+//        if(n!=0){
+//            cout << DyV(v,n) << endl;
+//            n=0;
+//        }else
+//            segir=false;
+//    }
+//    
+//    delete[] v;
+//}
--- EDA/va05.cpp
+++ EDA/va05.cpp
@@ -1,3 +1,223 @@
+//
+//  va05.cpp
+//  EDA
+//
+//  Created by Victor del Pino Castilla on 12/1/17.
+//  Copyright © 2017 Victor del Pino Castilla. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+void llenarVector(int n, int*c, int*v, istream& sin);
+int elegirCanciones(int n, int tiempoCinta, int* tiempos, int* puntuaciones);
+void elegirCancionesVA(int n, int k, int restnateA, int restanteB, int* tiempos, int* puntuaciones, bool* mejorElegidas, bool* elegidas, int& mejorSolucion, int solucion, int maxPuntuacion, int minTiempo,int& numero);
+void tratarSolucion(int& mejorSolucion, int solucion, int* mejorElegidas, int* elegidas, int n);
+bool esSolucion(int n , int k, int restanteA, int restanteB, int minTiempo);
+bool esPosible(int restanteA, int restanteB, int puntuacion, int maxPuntuacion, int n, int k, int mejorSolucion);
+
+void mergeSort(int p[], int c[], int a, int b);
+void mergeSort ( int p[],int c[], int n);
+void mezcla(int p[], int c[], int a, int m, int b );
+
+
+
+
+bool esSolucion(int n , int k, int restanteA, int restanteB, int minTiempo){
+    return (n==k + 1) || (
+                          (restanteA - minTiempo < 0)
+                          &&
+                          (restanteB - minTiempo < 0)
+                          );
+}
+
+bool esPosible(int restanteA, int restanteB, int solucion, int maxPuntuacion, int n, int k, int mejorSolucion, int minTiempo){
+    return ( restanteA >= 0
+            && restanteB >= 0
+            )
+    && (solucion + maxPuntuacion) > mejorSolucion;
+}
+
+
+void tratarSolucion(int& mejorSolucion, int solucion, bool* mejorElegidas, bool* elegidas, int n){
+    if(mejorSolucion < solucion){
+        mejorSolucion = solucion;
+//        for (int i = 0; i <= n; i++) {
+//            mejorElegidas[i] = elegidas[i];
+//        }
+    }
+}
+
+void elegirCancionesVA(int n, int k, int restanteA, int restanteB, int* tiempos, int* puntuaciones, bool* mejorElegidas, bool* elegidas, int& mejorSolucion, int solucion, int maxPuntuacion, int minTiempo,int& numero){
+    
+    elegidas[k]=true;
+    //if (n==k+1) cout<< "rama"<<endl;
+    
+    numero++;
+    maxPuntuacion -= puntuaciones[k];
+    //CaraA
+    restanteA -= tiempos[k];
+    
+    solucion += puntuaciones[k];
+    
+    if(esPosible(restanteA, restanteB, solucion, maxPuntuacion,n,k,mejorSolucion, minTiempo)){
+        if(esSolucion(n, k, restanteA, restanteB, minTiempo)){
+            tratarSolucion(mejorSolucion, solucion, mejorElegidas, elegidas, n);
+        }else{
+            elegirCancionesVA(n, k+1, restanteA, restanteB, tiempos, puntuaciones, mejorElegidas, elegidas, mejorSolucion, solucion, maxPuntuacion, minTiempo,numero);
+        }
+    }
+    
+    restanteA += tiempos[k];
+    
+    //CaraB
+    restanteB -= tiempos[k];
+    
+    if(esPosible(restanteA, restanteB, solucion, maxPuntuacion,n,k,mejorSolucion,minTiempo)){
+        if(esSolucion(n, k, restanteA, restanteB, minTiempo)){
+            tratarSolucion(mejorSolucion, solucion, mejorElegidas, elegidas, n);
+        }else{
+            elegirCancionesVA(n, k+1, restanteA, restanteB, tiempos, puntuaciones, mejorElegidas, elegidas, mejorSolucion, solucion, maxPuntuacion, minTiempo,numero);
+        }
+    }
+    
+    restanteB += tiempos[k];
+    solucion -= puntuaciones[k];
+    
+    
+    if(esSolucion(n, k, restanteA, restanteB, minTiempo)){
+        tratarSolucion(mejorSolucion, solucion, mejorElegidas, elegidas, n);
+    }else{
+        //No elegida
+        elegidas[k]=false;
+        elegirCancionesVA(n, k+1, restanteA, restanteB, tiempos, puntuaciones, mejorElegidas, elegidas, mejorSolucion, solucion, maxPuntuacion, minTiempo,numero);
+    }
+}
+
+
+void mezcla(int p[], int c[], int a, int m, int b ){
+    int *uP = new int[b-a+1];
+    int *uC = new int[b-a+1];
+    
+    int i, j, k;
+    
+    for ( k = a; k <= b; k++ ){
+        uP[k-a] = p[k];
+        uC[k-a] = c[k];
+    }
+
+    i = 0; j = m-a+1; k = a;
+    
+    while ( (i <= m-a) && (j <= b-a) ) {
+        if ( uP[i] > uP[j] ){
+            p[k] = uP[i];
+            c[k] = uC[i];
+            i = i + 1;
+        } else {
+            p[k] = uP[j];
+            c[k] = uC[j];
+            j = j + 1;
+        }
+        k = k + 1;
+    }
+    
+    while ( i <= m-a ) {
+        p[k] = uP[i];
+        c[k] = uC[i];
+        i = i+1;
+        k = k+1;
+    }
+    
+    while ( j <= b-a ) {
+        p[k] = uP[j];
+        c[k] = uC[j];
+        j = j+1;
+        k = k+1;
+    }
+    
+    delete[] uP;
+    delete[] uC;
+}
+
+void mergeSort(int p[], int c[], int a, int b) {
+    int m;
+    if ( a < b ) {
+        m = (a+b) / 2;
+        mergeSort(p, c, a, m);
+        mergeSort(p, c, m+1, b);
+        mezcla(p, c, a, m, b);
+    }
+}
+
+void mergeSort ( int p[],int c[], int n) {
+    mergeSort(p, c, 0, n-1);
+}
+
+    
+int elegirCanciones(int n, int tiempoCinta, int* tiempos, int* puntuaciones){
+    bool* elegidas = new bool[n];
+    bool* mejorElegidas = new bool[n];
+    int maxPuntuacion = 0;
+    int minTiempo = tiempoCinta;
+    int mejorSolucion = 0;
+    
+    mergeSort(puntuaciones, tiempos, n);
+    
+    for (int i = 0; i <  n; i++) {
+        maxPuntuacion += puntuaciones[i];
+        
+        if(tiempos[i] < minTiempo)
+            minTiempo = tiempos[i];
+        
+        elegidas[i]=false;
+        mejorElegidas[i]=false;
+    }
+    int numero=0;
+    elegirCancionesVA(n, 0, tiempoCinta, tiempoCinta, tiempos, puntuaciones, mejorElegidas, elegidas, mejorSolucion,0, maxPuntuacion, minTiempo,numero);
+    
+  //  cout << numero<<endl;
+    
+    delete [] elegidas;
+    delete [] mejorElegidas;
+    return mejorSolucion;
+}
+
+int main(){
+    int n;
+    int t;
+    int* c;
+    int* p;
+    cin >> n;
+    
+    while(n!=0){
+        cin >> t;
+        
+        c = new int[n];
+        p = new int[n];
+        
+        llenarVector(n,c,p,cin);
+        
+        cout << elegirCanciones(n,t,c,p) << endl;
+        
+        delete[] c;
+        delete[] p;
+        cin >> n;
+    }
+}
+
+void llenarVector(int n, int*c,int*v,istream& sin){
+    for (int i=0; i<n; i++) {
+        cin>>c[i];
+        cin>>v[i];
+    }
+}
+
+
+
+
+
 ////
 ////  va05.cpp
 ////  EDA
@@ -11,7 +231,101 @@
 //
 //using namespace std;
 //
-//void llenarVector(int n, int*c,int*v,istream& sin);
+//void llenarVector(int n, int*c, int*v, istream& sin);
+//int elegirCanciones(int n, int tiempoCinta, int* tiempos, int* puntuaciones);
+//void elegirCancionesVA(int n, int k, int restnateA, int restanteB, int* tiempos, int* puntuaciones, bool* mejorElegidas, bool* elegidas, int& mejorSolucion, int solucion, int maxPuntuacion, int minTiempo);
+//void tratarSolucion(int& mejorSolucion, int solucion, int* mejorElegidas, int* elegidas, int n);
+//bool esSolucion(int n , int k, int restanteA, int restanteB, int minTiempo);
+//bool esPosible(int restanteA, int restanteB, int puntuacion, int maxPuntuacion, int n, int k, int mejorSolucion);
+//
+//
+//bool esSolucion(int n , int k, int restanteA, int restanteB, int minTiempo){
+//    return (n==k+1) || ((restanteA - minTiempo < 0) && (restanteB - minTiempo < 0));}
+//
+//bool esPosible(int restanteA, int restanteB, int solucion, int maxPuntuacion, int n, int k, int mejorSolucion, int minTiempo){
+//    return ( restanteA >= 0
+//            && restanteB >= 0
+//            )
+//    && (solucion + maxPuntuacion) > mejorSolucion;
+//}
+//
+//
+//void tratarSolucion(int& mejorSolucion, int solucion, bool* mejorElegidas, bool* elegidas, int n){
+//    if(mejorSolucion < solucion){
+//        mejorSolucion = solucion;
+////        for (int i = 0; i <= n; i++) {
+////            mejorElegidas[i] = elegidas[i];
+////        }
+//    }
+//}
+//void elegirCancionesVA(int n, int k, int restanteA, int restanteB, int* tiempos, int* puntuaciones, bool* mejorElegidas, bool* elegidas, int& mejorSolucion, int solucion, int maxPuntuacion, int minTiempo){
+//
+//    elegidas[k]=true;
+//    maxPuntuacion -= puntuaciones[k];
+//    //CaraA
+//    restanteA -= tiempos[k];
+//    
+//    solucion += puntuaciones[k];
+//    
+//    if(esPosible(restanteA, restanteB, solucion, maxPuntuacion,n,k,mejorSolucion, minTiempo)){
+//        if(esSolucion(n, k, restanteA, restanteB, minTiempo)){
+//            tratarSolucion(mejorSolucion, solucion, mejorElegidas, elegidas, n);
+//        }else{
+//            elegirCancionesVA(n, k+1, restanteA, restanteB, tiempos, puntuaciones, mejorElegidas, elegidas, mejorSolucion, solucion, maxPuntuacion, minTiempo);
+//        }
+//    }
+//
+//    restanteA += tiempos[k];
+//
+//    //CaraB
+//    restanteB -= tiempos[k];
+//    
+//    if(esPosible(restanteA, restanteB, solucion, maxPuntuacion,n,k,mejorSolucion,minTiempo)){
+//        if(esSolucion(n, k, restanteA, restanteB, minTiempo)){
+//            tratarSolucion(mejorSolucion, solucion, mejorElegidas, elegidas, n);
+//        }else{
+//            elegirCancionesVA(n, k+1, restanteA, restanteB, tiempos, puntuaciones, mejorElegidas, elegidas, mejorSolucion, solucion, maxPuntuacion, minTiempo);
+//        }
+//    }
+//    
+//    restanteB += tiempos[k];
+//    solucion -= puntuaciones[k];
+//
+//    
+//    if(esPosible(restanteA, restanteB, solucion, maxPuntuacion,n,k,mejorSolucion,minTiempo)){
+//        if(esSolucion(n, k, restanteA, restanteB, minTiempo)){
+//            tratarSolucion(mejorSolucion, solucion, mejorElegidas, elegidas, n);
+//        }else{
+//            //No elegida
+//            elegidas[k]=false;
+//            elegirCancionesVA(n, k+1, restanteA, restanteB, tiempos, puntuaciones, mejorElegidas, elegidas, mejorSolucion, solucion, maxPuntuacion, minTiempo);
+//        }
+//    }
+//}
+//
+//int elegirCanciones(int n, int tiempoCinta, int* tiempos, int* puntuaciones){
+//    bool* elegidas = new bool[n];
+//    bool* mejorElegidas = new bool[n];
+//    int maxPuntuacion = 0;
+//    int minTiempo = tiempoCinta;
+//    int mejorSolucion = 0;
+//    
+//    for (int i = 0; i <  n; i++) {
+//            maxPuntuacion += puntuaciones[i];
+//
+//        if(tiempos[i] < minTiempo)
+//            minTiempo = tiempos[i];
+//
+//        elegidas[i]=false;
+//        mejorElegidas[i]=false;
+//    }
+//    
+//    elegirCancionesVA(n, 0, tiempoCinta, tiempoCinta, tiempos, puntuaciones, mejorElegidas, elegidas, mejorSolucion,0, maxPuntuacion, minTiempo);
+//    
+//    delete [] elegidas;
+//    delete [] mejorElegidas;
+//    return mejorSolucion;
+//}
 //
 //int main(){
 //    int n;
@@ -19,12 +333,21 @@
 //    int* c;
 //    int* p;
 //    cin >> n;
-//    cin >> t;
 //    
-//    llenarVector(n,c,p,cin);
-//    
-//    cout<< c[0];
-//    
+//    while(n!=0){
+//        cin >> t;
+//        
+//        c = new int[n];
+//        p = new int[n];
+//        
+//        llenarVector(n,c,p,cin);
+//        
+//        cout << elegirCanciones(n,t,c,p) << endl;
+//        
+//        delete[] c;
+//        delete[] p;
+//        cin >> n;
+//    }
 //}
 //
 //void llenarVector(int n, int*c,int*v,istream& sin){
