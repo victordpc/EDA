e1acecf9de7aa02900f81a1c9fd1c0f835b510b5
victordpc <victordpc@gmail.com>
2017-03-16 17:47:31 +0100

    Tema8 ejercicio 2

---
+++ EDA-2/EDA-2/Tema8/Tema08-01.cpp
@@ -0,0 +1,22 @@
+
+//#include <stack>
+//#include <iostream>
+//#include "string"
+//
+//using namespace std;
+//
+//
+//int main() {
+//	string car;
+//	getline(cin, car);
+//	while (car != ".") {
+//		comprobar(car);
+//		getline(cin, car);
+//	}
+//
+//}
+//
+//void comprobar(const string &car) {
+//
+//}
+//
