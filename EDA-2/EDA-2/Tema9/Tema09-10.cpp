d32318ec42fe6c9a746b849d91cd0b84cb740a76
victordpc <victordpc@gmail.com>
2017-05-05 18:44:28 +0200

    Otra forma de resolver el problema

--- EDA-2/EDA-2/Tema9/Tema09-10.cpp
+++ EDA-2/EDA-2/Tema9/Tema09-10.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <climits>
 #include "Arbin.h"
 #include "Tema09-10.h"
 
@@ -18,15 +19,15 @@ Arbin<T> leerMazmorra(const T& repVacio) {
 	}
 }
 
-void solucionaMazmorraRecursiva(Arbin<int> mazmorra, int &minimoDragones, int dragonesAcumulados, int &mejorPuerta) {
-	if (mazmorra.esVacio()) {
+void solucionaMazmorraRaizHojasRecursiva(Arbin<int> mazmorra, int &minimoDragones, int dragonesAcumulados, int &mejorPuerta) {
+	if (mazmorra.esVacio()|| dragonesAcumulados>minimoDragones) {
 		return;
 	} else if (1 == mazmorra.raiz()) {
-		solucionaMazmorraRecursiva(mazmorra.hijoIz(), minimoDragones, dragonesAcumulados + 1, mejorPuerta);
-		solucionaMazmorraRecursiva(mazmorra.hijoDr(), minimoDragones, dragonesAcumulados + 1, mejorPuerta);
+		solucionaMazmorraRaizHojasRecursiva(mazmorra.hijoIz(), minimoDragones, dragonesAcumulados + 1, mejorPuerta);
+		solucionaMazmorraRaizHojasRecursiva(mazmorra.hijoDr(), minimoDragones, dragonesAcumulados + 1, mejorPuerta);
 	} else if (2 == mazmorra.raiz() || 0 == mazmorra.raiz()) {
-		solucionaMazmorraRecursiva(mazmorra.hijoIz(), minimoDragones, dragonesAcumulados, mejorPuerta);
-		solucionaMazmorraRecursiva(mazmorra.hijoDr(), minimoDragones, dragonesAcumulados, mejorPuerta);
+		solucionaMazmorraRaizHojasRecursiva(mazmorra.hijoIz(), minimoDragones, dragonesAcumulados, mejorPuerta);
+		solucionaMazmorraRaizHojasRecursiva(mazmorra.hijoDr(), minimoDragones, dragonesAcumulados, mejorPuerta);
 	} else {
 		if (dragonesAcumulados < minimoDragones) {
 			mejorPuerta = mazmorra.raiz();
@@ -35,22 +36,60 @@ void solucionaMazmorraRecursiva(Arbin<int> mazmorra, int &minimoDragones, int dr
 	}
 }
 
-void solucionaMazmorra(Arbin<int> mazmorra) {
+void solucionaMazmorraRaizHojas(Arbin<int> mazmorra) {
 	int minimoDragrones = mazmorra.ContarNodos();
 	int mejorPuerta = 0;
 
-	solucionaMazmorraRecursiva(mazmorra, minimoDragrones, 0, mejorPuerta);
+	solucionaMazmorraRaizHojasRecursiva(mazmorra, minimoDragrones, 0, mejorPuerta);
 	cout << mejorPuerta << endl;
 }
 
-void entraMazmorra() {
+void solucionaMazmorraHojasRaizRecursiva(Arbin<int> mazmorra, int &minimoDragones, int &mejorPuerta) {
+	if (mazmorra.esVacio()) {
+		minimoDragones = INT_MAX;
+	}
+	else if (0 <= mazmorra.raiz() && mazmorra.raiz() <= 2) {
+		int dragonesIz = 0;
+		int dragonesDr = 0;
+		int puertaIz = 0;
+		int puertaDr = 0;
+
+		solucionaMazmorraHojasRaizRecursiva(mazmorra.hijoIz(), dragonesIz, puertaIz);
+		solucionaMazmorraHojasRaizRecursiva(mazmorra.hijoDr(), dragonesDr, puertaDr);
+
+		minimoDragones = (dragonesIz > dragonesDr) ? dragonesDr : dragonesIz;
+		minimoDragones = (mazmorra.raiz() == 1) ? minimoDragones + 1 : minimoDragones;
+
+		mejorPuerta = (dragonesIz > dragonesDr) ? puertaDr : puertaIz;
+	}
+	else {
+		mejorPuerta = mazmorra.raiz();
+		minimoDragones = 0;
+	}
+}
+
+
+
+void solucionaMazmorraHojasRaiz(Arbin<int> mazmorra) {
+	int minimoDragrones = 0;
+	int mejorPuerta = 0;
+
+	solucionaMazmorraHojasRaizRecursiva(mazmorra, minimoDragrones,mejorPuerta);
+	cout << mejorPuerta << endl;
+}
+
+
+void entraMazmorra(bool raiz_a_hojas) {
 	Arbin<int> mazmorra = leerMazmorra(-1);
-	solucionaMazmorra(mazmorra);
+	if (raiz_a_hojas)
+		solucionaMazmorraRaizHojas(mazmorra);
+	else
+		solucionaMazmorraHojasRaiz(mazmorra);
 }
 
 void ejercicio0910() {
 	int numMazmorras;
 	cin >> numMazmorras;
 	for (int i = 0; i < numMazmorras; i++)
-		entraMazmorra();
+		entraMazmorra(false);
 }
