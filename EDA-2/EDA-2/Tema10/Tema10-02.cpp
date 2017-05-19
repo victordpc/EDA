be6dd069538a6728f8a074313990c9788771a9a8
victordpc <victordpc@gmail.com>
2017-05-19 18:54:32 +0200

    Tema 10 y 11

---
+++ EDA-2/EDA-2/Tema10/Tema10-02.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <list>
+#include <string>
+
+#include "TreeMap.h"
+
+using namespace std;
+
+void resuelveSerie() {
+	int numCap = 0;
+	int maxNodos = 0;
+	TreeMap<int, int> series;
+
+	cin >> numCap;
+	while (numCap-- > 0) {
+
+	}
+
+}
+
+void ejercicio1002() {
+	int casos = 0;
+	cin >> casos;
+
+	while (casos-- != 0) {
+		resuelveSerie();
+	}
+}
---
+++ EDA-2/EDA-2/Tema10/main.cpp
@@ -0,0 +1,9 @@
+#include "Tema10-03.h"
+
+
+using namespace std;
+
+int main() {
+	ejercicio1003();
+	return 0;
+}
